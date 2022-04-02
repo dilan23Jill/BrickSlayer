8e6b5d8e4401fbf38cb5d54b788a51a6b194d2d3
dilan23Jill <muzicdilan@gmail.com>
2022-04-02 02:36:39 +0200

    starting page

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* function loadGame() {
+    var elem = document.getElementById("info");   
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+      }
+    }
+  } */
+
+
