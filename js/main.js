ea3b400c6726c0539f64a6db300885d6c09782f9
dilan23Jill <muzicdilan@gmail.com>
2022-04-03 18:23:46 +0200

    v 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('.game-frame-two').hide();
+  /*  $('.game-frame-two').hide();
   $('canvas').hide();
 
   $('.header').on('click', function () {
@@ -17,5 +17,80 @@ $(document).ready(function () {
   $('.character').dblclick(function () {
     $('.game-frame-two').fadeOut(400);
     $('canvas').delay(400).fadeIn(600);
-  });
+  }); */
 });
+
+var sword = new Image();
+sword.src = './img/Kyojuro-sword.png';
+
+var character = new Image();
+character.src = './img/Kyojuro.webp';
+
+var CharaterHeight = character.height / 7;
+var CharaterWidth = character.width / 7;
+let rot = 0;
+var startW = canvas.width / 2 - 10;
+var startH = canvas.height - CharaterHeight - 100;
+
+let brick_arr = [];
+for(var i=0;){
+  for(){
+
+  }
+}
+
+
+function draw() {
+  var x = startW;
+  var y = startH;
+  var dx = -2;
+  var dy = -4;
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  ctx.drawImage(sword, x, y, 20, 90);
+  ctx.drawImage(
+    character,
+    canvas.width / 2 - CharaterWidth / 2,
+    canvas.height - CharaterHeight,
+    CharaterWidth,
+    CharaterHeight
+  );
+
+  function init() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    return setInterval(move, 10);
+  }
+
+  function move() {
+          /* ctx.translate(x + 20 * 0.5, y + 90 * 0.5);
+            ctx.rotate(-.01);
+            ctx.translate(-(x + 20 * 0.5), -(y + 90 * 0.5));rot++; 
+            */
+    ctx.clearRect(x, y, 20, 90);
+    x += dx;
+    y += dy;
+          /* if (
+            x + 20 > canvas.width / 2 - CharaterWidth / 2 &&
+            x < canvas.width / 2 + CharaterWidth / 2 &&
+            y + 90 > canvas.height - CharaterHeight
+          ) {
+            ctx.drawImage(
+              character,
+              canvas.width / 2 - CharaterWidth / 2,
+              canvas.height - CharaterHeight,
+              CharaterWidth,
+              CharaterHeight
+            );
+          } */
+    if (x + 20 > canvas.width || x <= -5) {
+      dx = -dx;
+    }
+    if (y + 90 > canvas.height || y <= 0) {
+      dy = -dy;
+    }
+
+    ctx.drawImage(sword, x, y, 20, 90);
+  }
+  init();
+}
